d213f13419084c3f64d64c85bdbba71931d223ef
RagingComputer <raging@ragingcomputer.com>
2016-06-19 17:51:28 -0500

    Update function handleData

--- hikvision.js
+++ hikvision.js
@@ -20,6 +20,8 @@ var hikvision = function(options) {
 	this.client = this.connect(options)
 	if (options.log)	TRACE = options.log;
 	BASEURI = 'http://' + options.host + ':' + options.port
+	this.activeEvents	= { };
+	this.triggerActive = false
 };
 
 util.inherits(hikvision, events.EventEmitter);
@@ -176,24 +178,86 @@ hikvision.prototype.nightProfile = function () {
 
 // Handle alarms
 function handleData(self, data) {
-	if (TRACE)	console.log('Data: ' + data.toString());
 	parser.parseString(data, function(err, result) {
 		if (result) {
-	        	var code = result['EventNotificationAlert']['eventType'][0]
-	        	var action = result['EventNotificationAlert']['eventState'][0]
-	        	var index = parseInt(result['EventNotificationAlert']['inputIOPortID'][0])
-	        	var count = parseInt(result['EventNotificationAlert']['activePostCount'][0])
-			if (count == 1) { 
-				if (code === 'IO')		code = 'AlarmLocal';
-				if (code === 'VMD')		code = 'VideoMotion';
-				if (code === 'videoloss')	code = 'VideoLoss';
-				if (code === 'shelteralarm')	code = 'VideoBlind';
-				if (action === 'active')	action = 'Start'
-				if (action === 'inactive')	action = 'Stop'
+			var code = result['EventNotificationAlert']['eventType'][0]
+			var action = result['EventNotificationAlert']['eventState'][0]
+			var index = parseInt(result['EventNotificationAlert']['channelID'][0])
+			var count = parseInt(result['EventNotificationAlert']['activePostCount'][0])
+
+			// give codes returned by camera prettier and standardized description
+			if (code === 'IO')            code = 'AlarmLocal';
+			if (code === 'VMD')           code = 'VideoMotion';
+			if (code === 'linedetection') code = 'LineDetection';
+			if (code === 'videoloss')     code = 'VideoLoss';
+			if (code === 'shelteralarm')  code = 'VideoBlind';
+			if (action === 'active')    action = 'Start'
+			if (action === 'inactive')  action = 'Stop'
+
+			// create and event identifier for each recieved event
+			// This allows multiple detection types with multiple indexes for DVR or multihead devices
+			var eventIdentifier = code + index
+
+			// Count 0 seems to indicate everything is fine and nothing is wrong, used as a heartbeat
+			// if triggerActive is true, lets step through the activeEvents
+			// If activeEvents has something, lets end those events and clear activeEvents and reset triggerActive
+			if (count == 0) {
+				if (self.triggerActive == true) {
+					for(var i in self.activeEvents) {
+						if(self.activeEvents.hasOwnProperty(i)){
+							var eventDetails = self.activeEvents[i]
+							if (TRACE)	console.log('Ending Event: ' + i + ' - ' + eventDetails["code"] + ' - ' + ((Date.now() - eventDetails["lasttimestamp"])/1000));
+							self.emit("alarm", eventDetails["code"],'Stop', eventDetails["index"]);
+						}
+					}
+					self.activeEvents	= {};
+					self.triggerActive = false
+
+				} else {
+					// should be the most common result
+					// Nothing interesting happening and we haven't seen any events
+					if (TRACE)	self.emit("alarm", code,action,index);
+				}
+			}
+
+			// if the first instance of an eventIdentifier, lets emit it, 
+			// add to activeEvents and set triggerActive
+			else if (typeof self.activeEvents[eventIdentifier] == 'undefined' || self.activeEvents[eventIdentifier] == null){
+				var eventDetails = { }
+				eventDetails["code"] = code
+				eventDetails["index"] = index
+				eventDetails["lasttimestamp"] = Date.now();
+
+				self.activeEvents[eventIdentifier] = eventDetails
 				self.emit("alarm", code,action,index);
+				self.triggerActive = true
+
+			// known active events
+			} else {
+				if (TRACE)	console.log('    Skipped Event: ' + code + ' ' + action + ' ' + index + ' ' + count );
+
+				// Update lasttimestamp
+				var eventDetails = { }
+				eventDetails["code"] = code
+				eventDetails["index"] = index
+				eventDetails["lasttimestamp"] = Date.now();
+				self.activeEvents[eventIdentifier] = eventDetails
+
+				// step through activeEvents
+				// if we haven't seen it in more than 2 seconds, lets end it and remove from activeEvents
+				for(var i in self.activeEvents) {
+					if(self.activeEvents.hasOwnProperty(i)){
+						var eventDetails = self.activeEvents[i]
+						if (((Date.now() - eventDetails["lasttimestamp"])/1000) > 2) {
+							if (TRACE)	console.log('    Ending Event: ' + i + ' - ' + eventDetails["code"] + ' - ' + ((Date.now() - eventDetails["lasttimestamp"])/1000));
+							self.emit("alarm", eventDetails["code"],'Stop', eventDetails["index"]);
+							delete self.activeEvents[i]
+						}
+					}
+				}
 			}
 		}
-    	});
+	});
 }
 
 // Handle connection
